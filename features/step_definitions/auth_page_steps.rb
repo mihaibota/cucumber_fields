784aa8707b0bd940a664ef1b480c7f0559bad226
mihai.bota <mihai.bota@P5446.ro.softvision.global>
2018-12-04 10:43:15 +0200

    Reworked ex 1 after the review

--- features/step_definitions/auth_page_steps.rb
+++ features/step_definitions/auth_page_steps.rb
@@ -4,26 +4,6 @@ And(/^the 'authentication' page should become visible$/) do
   end
 end
 
-When(/^I enter the (.*) mail adress in the email field$/) do |name|
-  @authentication_page.user_email_txt = name
-end
-
-When(/^I enter the (.*) password in the password field$/) do |password|
-  @authentication_page.user_password_txt = password
-end
-
-When(/^I click the "Sign in" button on the Authentification page$/) do
-  @authentication_page.sing_in_btn
-end
-
-Then(/^The (.*) error should be displayed$/) do |error|
-  if @authentication_page.error_message_specific_txt != error
-    puts "<" + @authentication_page.error_message_specific_txt + "> was discovered instead of: " + error
-  end
-  @authentication_page.error_message_specific_txt.should == error
-end
-
-
 Then(/^on the Auth page I enter the wrong credentials and look for the errors$/) do |table|
   data = table.hashes
   data.each do |element|
